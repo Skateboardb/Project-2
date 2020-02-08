1b7733061912301dced2de63a077e526e90f1f2c
Branford Shaw <branford.shaw@gmail.com>
2020-02-08 15:12:44 -0500

    added question data and fixed syntax errors

--- models/schema.sql
+++ models/schema.sql
@@ -4,30 +4,30 @@ USE beer_db;
 CREATE TABLE IF NOT EXISTS users
 (
  	id int NOT NULL AUTO_INCREMENT,
-	userFirst varchar(255) NOT NULL,
-    userLast varchar(255) NOT NULL,
-	beer varchar(255) NOT NULL,
+	userFirst varchar(255),
+    userLast varchar(255),
+	beer varchar(255),
     EmailAddress varchar(20),
 	PRIMARY KEY (id)
 );
 CREATE TABLE IF NOT EXISTS beers
  (  
     id int NOT NULL AUTO_INCREMENT,
-    style varchar(255) NOT NULL,
-    description VARCHAR (500) NOT NULL,
+    style varchar(255) ,
+    description VARCHAR (500),
     PRIMARY KEY (id)
  );
  CREATE TABLE IF NOT EXISTS questions    
  (  
     id int NOT NULL AUTO_INCREMENT,
-    question VARCHAR (1000) NOT NULL,
+    question VARCHAR (1000),
     PRIMARY KEY (id)
      );
 CREATE TABLE IF NOT EXISTS answers
 (
  id INT NOT NULL AUTO_INCREMENT,
  question_id INT NOT NULL,
-    answer VARCHAR (255) NOT NULL,
+    answer VARCHAR (255) ,
     PRIMARY KEY (id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
     );
