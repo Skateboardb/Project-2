5ff343e18e34da55907934bf73dc3d46a15ac3cc
Branford Shaw <branford.shaw@gmail.com>
2020-02-08 22:34:08 -0500

    working on configuring handlebars

--- models/beer.js
+++ models/beer.js
@@ -1,9 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
-  var beer = sequelize.define("beer", {
-    style: DataTypes.STRING,
-    description: DataTypes.TEXT
-  }, {
-    timestamps: false
-  });
-  return beer;
+	var beer = sequelize.define(
+		"beer",
+		{
+			style: { type: DataTypes.STRING, allowNull: false },
+			description: { type: DataTypes.TEXT, allowNull: false }
+		},
+		{
+			timestamps: false
+		}
+	);
+	return beer;
 };
--- server.js
+++ server.js
@@ -14,34 +14,34 @@ app.use(express.static("public"));
 
 // Handlebars
 app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+	"handlebars",
+	exphbs({
+		defaultLayout: "main"
+	})
 );
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./controllers/apiRoutes")(app);
+// require("./controllers/beers-controller")(app);
 
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+	syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+	app.listen(PORT, function() {
+		console.log(
+			"==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+			PORT,
+			PORT
+		);
+	});
 });
 
 module.exports = app;
